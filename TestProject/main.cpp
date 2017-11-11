b4cc6bd6af43230914c71350e344f9680d4d4e39
VladislavSheptunov <sheptunov.vld@gmail.com>
2017-11-11 17:43:34 +0300

    Bug fix

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -6,9 +6,13 @@
 #include <stdio.h>
 
 #define Range(a,x,b) (x>=a)&&(x<=b)
+#define ItNUMinHEX(x) (	(x >=0x30) && (x <=0x39) || \
+						(x >=0x41) && (x <=0x46) || \
+						(x >=0x61) && (x <=0x66) )
+#define MaskCRC(x) (x == 0x58) || (x == 0x78)
 
 void print_request(unsigned char* buff, int size) {
-	printf("Out:");
+	printf("Output:");
 	for (int i = 0; i < size; ++i) {
 		printf(" ");
 		if (Range(0x00, buff[i], 0x09))
@@ -19,52 +23,59 @@ void print_request(unsigned char* buff, int size) {
 	printf("\n");
 }
 
-void checksum(unsigned char* buff, int size) {
-	//unsigned char checksum = 0;
-	//unsigned char *checksum_buff = (unsigned char*)calloc(size, sizeof(unsigned char));
-	//int j = 0, size_checksum_buff = size - 3;
-	//for (int i = 1; i < size - 3; ++i) {
-	//	if (buff[i] == 0x7E || buff[i] == 0x5D) {
-	//		size_checksum_buff++;
-	//		checksum_buff = (unsigned char*)realloc(checksum_buff, size_checksum_buff);
-	//		checksum_buff[j] = 0x5D;
-	//		j++;
-	//		checksum_buff[j] = buff[i]^0x20;
-	//	}
-	//	else {
-	//		checksum_buff[j] = buff[i];
-	//	}
-	//	j++;
-	//}
+unsigned char two_number_toHEX(char left, char right) {
+	return (unsigned char)((left << 4) | right);
+}
 
-	//for (int i = 0; i < size_checksum_buff; ++i) {
-	//	printf(" ");
-	//	if (Range(0x00, checksum_buff[i], 0x09))
-	//		printf("%X%X", 0, buff[i]);
-	//	else
-	//		printf("%X", checksum_buff[i]);
-	//}
+unsigned char toHEX(char ch) {
+	if (Range(0x30, ch, 0x39)) {
+		ch -= 0x30;
+	}
+	else if (Range(0x41, ch, 0x46)) {
+		ch -= 0x37;
+	}
+	else if (Range(0x61, ch, 0x66)) {
+		ch -= 0x57;
+	}
+	else if (MaskCRC(ch)) {
+		ch = 0x58;
+	}
 
+	return ch;
+}
 
+void request_with_CRC(char *str) {
+	int size_request = strlen(str);
+	int size_request_hex = size_request / 2;
+
+	int i = 0, j = 0;
+	unsigned char *requesthex = (unsigned char*)calloc(size_request_hex, sizeof(unsigned char));
+
+	while (size_request - i != 0) {
+		requesthex[j] = two_number_toHEX(toHEX(str[i]), toHEX(str[i + 1]));
+		i += 2;
+		j++;
+	}
 	unsigned char checksum = 0;
-	unsigned char *checksum_buff = (unsigned char*)calloc(size, sizeof(unsigned char));
-	int j = 1, size_checksum_buff = size;
-	checksum_buff[0] = buff[0];
-	for (int i = 1; i < size - 2; ++i) {
-		if (buff[i] == 0x7E || buff[i] == 0x5D) {
+	unsigned char *checksum_buff = (unsigned char*)calloc(size_request_hex, sizeof(unsigned char));
+	int size_checksum_buff = size_request_hex;
+	j = 1;
+	checksum_buff[0] = requesthex[0];
+	for (int i = 1; i < size_request_hex - 2; ++i) {
+		if (requesthex[i] == 0x7E || requesthex[i] == 0x5D) {
 			size_checksum_buff++;
 			checksum_buff = (unsigned char*)realloc(checksum_buff, size_checksum_buff);
 			checksum_buff[j] = 0x5D;
 			j++;
-			checksum_buff[j] = buff[i] ^ 0x20;
+			checksum_buff[j] = requesthex[i] ^ 0x20;
 		}
 		else {
-			checksum_buff[j] = buff[i];
+			checksum_buff[j] = requesthex[i];
 		}
 		j++;
 	}
-
-	checksum_buff[size_checksum_buff - 1] = buff[size -1];
+	checksum_buff[size_checksum_buff - 1] = requesthex[size_request_hex - 1];
+	free(requesthex);
 
 	j = 1;
 	while (j != size_checksum_buff - 2) {
@@ -78,105 +89,95 @@ void checksum(unsigned char* buff, int size) {
 	free(checksum_buff);
 }
 
-char check_reqbuff(unsigned char *reqbuff, int size) {
-	if (reqbuff[0] == 0x7E && reqbuff[size - 1] == 0x7E)
-		return 0;
-	else
-		return -1;
-
-	for (int i = 0; i < size; ++i) {
-		if ((Range(48, reqbuff[i], 57) || Range(65, reqbuff[i], 70)) == 0) {
-			if (i != size -2 )
-				return -2;
+char check_request(char *str) {
+	int size = strlen(str);
+	if (size >= 7 && size % 2 == 0) {
+		if ((size > 6) && two_number_toHEX(toHEX(str[0]), toHEX(str[1])) == 0x7E &&
+			two_number_toHEX(toHEX(str[size - 2]), toHEX(str[size - 1])) == 0x7E &&
+			toHEX(str[size - 4]) == 0x58 && toHEX(str[size - 3] == 0x58)
+			) {
+			return 0;
 		}
+		else {
+			return -2;
+		}
+	}
+	else {
+		return -1;
 	}
 }
 
-
-char conversion_request(char *str) {
+void delite_space(char* str) {
 	int i, j;
-	int size = strlen(str),
-		num_space = 0;
-	int ret = 0;
-	// removing spaces and conversion from ASCI to number
-	for (i = 0, j = 0; str[i] != '\0'; i++) {
-		if (str[i] != ' ') {
-			if (Range(48, str[i], 57)) {
-				str[j++] = str[i] - 48;
-			}
-			else if (Range(65, str[i], 70)) {
-				str[j++] = str[i] - 55;
-			}
-			else {
-				str[j++] = str[i];
-			}
-		}
-		else {
-			num_space++;
-		}
-	}
-	str[j] = '\0';
+	int size = strlen(str);
+	char* bufftmp = (char*)calloc(size, sizeof(char));
+	// removing spaces 
+	for (i = 0, j = 0; i < size; i++)
+		if (str[i] != ' ') 
+			bufftmp[j++] = str[i];
+	bufftmp[j] = '\0';
+	memset(str, -1, size);
+	memcpy(str, bufftmp, size);
 
-	size -= num_space;
-
-	int size_reqbuff = size % 2 == 0 ? size / 2 : size / 2 + 1;
-	unsigned char *reqbuff = (unsigned char*)calloc(size_reqbuff, sizeof(unsigned char));
-
-	i = 0;
-	j = 0;
-	while (size - i != 0) {
-		reqbuff[j] = (unsigned char)((str[i] << 4) | str[i + 1]);
-		i += 2;
-		j++;
-	}
-
-	ret = check_reqbuff(reqbuff, size_reqbuff);
-
-	if (ret == 0) {
-		checksum(reqbuff, size_reqbuff);
-	}
-	else {
-		free(reqbuff);
-		return ret;
-	}
-	return 0;
+	free(bufftmp);
 }
 
 char analysis_request(char* str) {
 	bool flag = false;
+	char ret = 0;
 	for (int i = 0; i < strlen(str); ++i) {
-		if (Range(48, str[i], 57)) {
-			flag = true;
-		} else if (Range(65, str[i], 70)) {
-			flag = true;
-		} else if (str[i] == ' ' || str[i] == 'X') {
+		if (ItNUMinHEX(str[i]) || str[i] == ' ' || MaskCRC(str[i])) {
 			flag = true;
 		}
 		else {
 			flag = false;
 		}
 
-		if (flag == false)
-			return -3;
+		if (flag == false) {
+			ret = -1;
+			break;
+		}
 	}
-	return 0;
+	return ret;
 }
 
 int main(int argc, char* argv) {
-	char request[128];
+	char request[256];
+	int ret = 0;
 	printf("Checksum calculation\n\n");
+	printf("Input Format: 7E HEAD CRC 7E\nAttention! In request CRC = XX\n\n\n");
 
 	while (true) {
 		lable:
-		printf("Input request(in request CRC = XX): ");
+		printf("Input: ");
 		gets_s(request);
 
-		if (analysis_request(request) != 0)
+		if (analysis_request(request) != 0) {
+			printf("Wrong format!\n");
 			goto lable;
+		}
+	
+		delite_space(request);
+		ret = check_request(request);
 
-		if (conversion_request(request) != 0)
-			goto lable;
-
+		if (ret != 0) {
+			switch (ret)
+			{
+			case -1:
+				printf("Length error!\n");
+				goto lable;
+			case -2:
+				printf("Flag or checksum format error!\n");
+				goto lable;
+			default:
+				break;
+			}
+		}
+		else {
+			request_with_CRC(request);
+			memset(request, -1, 256);
+		}
+		
 		printf("To continue, press any key. To exit press ESC\n\n");
 
 		if (_getch() == 27)
